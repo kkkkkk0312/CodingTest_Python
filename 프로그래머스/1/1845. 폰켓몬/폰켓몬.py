e0392ff0c6587b61aedfdaf9e60b0326a4db5253
kkkkkk0312 <97078660+kkkkkk0312@users.noreply.github.com>
2024-01-22 23:03:36 +0900

    [level 1] Title: 폰켓몬, Time: 0.37 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,11 @@
+def solution(nums):
+    num=len(nums)/2
+    num_cnt=len(set(nums))
+    answer = 0
+    cnt=0
+    x=[]
+    if num>num_cnt:
+        return num_cnt
+    else:
+        return num
+    
