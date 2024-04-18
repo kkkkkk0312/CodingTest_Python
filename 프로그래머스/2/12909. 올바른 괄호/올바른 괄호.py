44884761e404f793c9a2da829640f7175ff82b66
kkkkkk0312 <97078660+kkkkkk0312@users.noreply.github.com>
2024-04-18 15:48:51 +0900

    [level 2] Title: 올바른 괄호, Time: 12.97 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,16 @@
+def solution(s):
+    cnt=0
+
+    for i in s:
+        if i=='(':
+            cnt+=1
+        else:
+            cnt-=1
+        if cnt<0:
+            break
+            return False
+    if cnt==0:
+        return True
+    else:
+        return False   
+        
