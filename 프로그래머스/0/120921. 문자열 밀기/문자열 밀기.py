c9a08a1301989445ea076fe5156e7b984b689258
kkkkkk0312 <97078660+kkkkkk0312@users.noreply.github.com>
2025-03-07 16:44:47 +0900

    [level 0] Title: 문자열 밀기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.py
@@ -1,11 +1,3 @@
 def solution(A, B):
-    answer = 0
-    if A==B:
-        return 0
-    for i in range(1,len(A)+1):
-        answer+=1
-        print(A[-i:]+A[:-i])
-        if (A[-i:]+A[:-i])==B:
-            return answer
-    return -1   
+    return (B*2).find(A)
     
